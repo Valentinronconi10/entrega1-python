0e879e5de25852879472ee7c49f0f9c1567c1095
Valentin Ronconi <valentin.ronconi10@gmail.com>
2026-03-30 20:22:42 -0300

    .

--- practica 2/ejercicio_2.py
+++ practica 2/ejercicio_2.py
@@ -13,16 +13,28 @@ duracion_total = 0
 
 sumaMinutos = 0
 sumaSegundos = 0
+max = -1
+min = 99999
 for i in range(len(playlist)):
     minutos,segundos = (playlist[i]["duration"].split(":"))
     minutos = int(minutos)
     segundos = int(segundos)
     sumaMinutos += minutos
     sumaSegundos += segundos
+    if (minutos > max):
+        max = minutos
+        max_segundos = segundos
+        cancion_max = playlist[i]["title"]
+    else:
+        if (minutos < min):
+            min = minutos
+            min_segundos = segundos
+            cancion_min = playlist[i]["title"]
 
 minutos_extra = sumaSegundos // 60
 sumaMinutos += minutos_extra
 sumaSegundos = sumaSegundos % 60
 print(f"Minutos: {sumaMinutos}")
 print(f"Segundos: {sumaSegundos}")
-
+print(f"La cancion mas larga es: '{cancion_max}' - {max} : {max_segundos:02d}")
+print(f"La cancion mas corta es: '{cancion_min}' - {min} : {min_segundos:02d} ")
